86df7017bacefbf96f19909a21f29bba141dcefe
Johnny Hauser <johnnyhauser@gmail.com>
2017-09-20 13:24:41 -0500

    use es6 and minor refactor

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-'use strict'
-
-var compose = function (middleware) {
+const composeMiddleware = middleware => {
   if (!Array.isArray(middleware)) {
     throw new TypeError('middleware stack must be an array')
   }
 
-  middleware.forEach(function (fn) {
-    if (typeof fn !== 'function') {
+  middleware.forEach(shouldBeFn => {
+    if (typeof shouldBeFn !== 'function') {
       throw new TypeError('middleware must be composed of functions')
     }
   })
 
-  return function (incoming, next) {
-    var dispatch = function (i) {
-      try {
-        var fn = i === middleware.length ? next : middleware[i]
-        var called
-        var result = !fn ? incoming : fn(incoming, function next (nextIncoming) {
+  return function composedMiddleware (incoming, next) {
+    const dispatch = i => Promise.resolve().then(() => {
+      const fn = i === middleware.length ? next : middleware[i]
+      let called
+      return !fn
+        ? incoming
+        : fn(incoming, function next (nextIncoming) {
           if (called) { throw new Error('next() called multiple times') }
           called = true
           incoming = nextIncoming
           return dispatch(i + 1)
         })
-        return Promise.resolve(result)
-      } catch (err) {
-        return Promise.reject(err)
-      }
-    }
+    })
     return dispatch(0)
   }
 }
 
-module.exports = compose
+export default composeMiddleware
