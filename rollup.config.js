a1cf439aaaa73006c703723f9425a35fdb889e20
Johnny Hauser <johnnyhauser@gmail.com>
2017-09-20 13:35:24 -0500

    rollup build

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+
+const Plugins = () => [
+  resolve({
+    module: true, browser: true, jsnext: true, main: true, extensions: [ '.js', '.json' ]
+  }),
+  commonjs()
+]
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'build/index.cjs.js',
+      format: 'cjs'
+    },
+    plugins: Plugins()
+  }
+  {
+    input: 'src/index.test.js',
+    output: {
+      file: 'build/index.test.js',
+      format: 'iife'
+    },
+    plugins: Plugins()
+  }
+]
